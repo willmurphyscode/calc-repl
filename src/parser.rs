409f2db5ca0939890c60da490e8580ed63003bc6
Will Murphy <willmurphyscode@gmail.com>
2017-12-02 13:50:36 -0500

    Parse left parens
    We have our parser combinator library included,
    and now we've written a simple parse rule - if the
    incoming slice is exactly the byte for a left
    paren, transform it to the left paren type.

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,16 @@
 
 use nom;
+use parse_node::{ParseNode,Opcode};
 
-named!(hello_recognizer, tag!("hello"));
+//This is a super helpful example
+// https://github.com/Rydgel/monkey-rust/blob/32d6db16c6b9c99202deafa8b36175f50f6522af/lib/lexer/mod.rs
+
+named!(left_paren<&[u8], ParseNode>, 
+    do_parse!(tag!("(") >> (ParseNode::LeftParen)) // parentheses around `ParseNode::LeftParen` are important
+    // you'll get a mysterious syntax error from nom macro expansions without them.
+);
 
 pub fn parse() {
-    let hello =&b"hello"[..];
-    let result = hello_recognizer(hello);
-    println!("{:?}", result);
+    let a = left_paren(&b"("[..]);
+    println!("{:?}{}", result, a.to_result().expect("failed to parse"));
 }
