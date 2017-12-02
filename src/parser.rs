ef02c9b56eed641ca66370ce0dcbc173c46da401
Will Murphy <willmurphyscode@gmail.com>
2017-12-02 14:17:19 -0500

    parse everything but values

--- src/parser.rs
+++ src/parser.rs
@@ -14,6 +14,22 @@ named!(right_paren<&[u8], Token>,
     do_parse!(tag!(")") >> (Token::RightParen))
 );
 
+named!(addition_sign<&[u8], Token>,
+    do_parse!(tag!("+") >> (Token::Operator(Opcode::Add)))
+);
+
+named!(subtraction_sign<&[u8], Token>,
+    do_parse!(tag!("-") >> (Token::Operator(Opcode::Subtract)))
+);
+
+named!(multiplication_sign<&[u8], Token>,
+    do_parse!(tag!("*") >> (Token::Operator(Opcode::Multiply)))
+);
+
+named!(division_sign<&[u8], Token>,
+    do_parse!(tag!("/") >> (Token::Operator(Opcode::Divide)))
+);
+
 pub fn parse() {
     let a = left_paren(&b"("[..]);
     println!("{}", left_paren(&b"("[..]).to_result().expect("failed to parse"));
@@ -22,4 +38,29 @@ pub fn parse() {
 #[test]
 fn left_paren_parser() {
     assert!(left_paren(&b"("[..]).to_result().expect("failed to parse left paren") == Token::LeftParen);
+}
+
+#[test]
+fn right_paren_parser() {
+    assert!(right_paren(&b")"[..]).to_result().expect("failed to parse right paren") == Token::RightParen);
+}
+
+#[test]
+fn addition_sign_parser() {
+    assert!(addition_sign(&b"+"[..]).to_result().expect("failed to parse addition sign") == Token::Operator(Opcode::Add));
+}
+
+#[test]
+fn subtraction_sign_parser() {
+    assert!(subtraction_sign(&b"-"[..]).to_result().expect("failed to parse subtraction sign") == Token::Operator(Opcode::Subtract));
+}
+
+#[test]
+fn multiplication_sign_parser() {
+    assert!(multiplication_sign(&b"*"[..]).to_result().expect("failed to parse multiplication sign") == Token::Operator(Opcode::Multiply));
+}
+
+#[test]
+fn division_sign_parser() {
+    assert!(division_sign(&b"/"[..]).to_result().expect("failed to parse division sign") == Token::Operator(Opcode::Divide));
 }
