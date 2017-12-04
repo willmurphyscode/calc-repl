f25140c7bd14bef087acc1b968c14734469ee998
Will Murphy <willmurphyscode@gmail.com>
2017-12-04 06:32:09 -0500

    Implement shift
    We're implementing a shift-reduce parser. We have two halves: a stack
    of tokens we've already seen, and a slice of tokens we haven't seen
    yet.
    
    There are two basic operations: Shift, which takes the first item in
    the unseen tokens and pushes it onto the stack, and reduce, which
    simplifies the stack by applying operators to operands that are
    already on the stack.
    
    This commit is my first pass at implementing shift.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -4,4 +4,25 @@ use runtime_error::RuntimeError;
 
 pub fn eval(tokens: Vec<Token>) -> Result<isize, RuntimeError> {
     Err(RuntimeError{})
+
+    // shift reduce parsing
+    // two operations
+    // shift: advance our position in the array of tokens by one,
+    // and push the token that's now to our left onto a stack.
+    // reduce: reduce operands that are on the left side of the stack
+    // by applying an operator, and replace on the stack the result of the
+    // reduction
+}
+
+fn shift<'a>(stack: &'a mut Vec<&'a Token>, remaining: &'a [&'a Token]) {
+    if(remaining.len() > 0) {
+        stack.push(remaining[0]);
+    }
+}
+
+fn reduce<'a>(stack: &mut Vec<&Token>, slice: &'a [&'a Token]) -> &'a [&'a Token] {
+    // this accepts a slice of tokens
+    // that starts and ends with parentheses, and includes only operands
+    // and operators
+    slice
 }
