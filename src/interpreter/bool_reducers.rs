b473a3b565a980d41780489399f56786e4019b6e
Will Murphy <willmurphyscode@gmail.com>
2018-02-09 07:16:13 -0500

    or reducer working and under test

--- src/interpreter/bool_reducers.rs
+++ src/interpreter/bool_reducers.rs
@@ -13,7 +13,14 @@ pub fn reduce_and(stack: &mut Vec<Token>) -> Result<Token, RuntimeError> {
 }
 
 pub fn reduce_or(stack: &mut Vec<Token>) -> Result<Token, RuntimeError> {
-    unimplemented!()
+    let bool_result = unwrap_bool_token_vec(stack);
+    if let Ok(bool_vec) = bool_result {
+        Ok(Token::Operand(Type::Bool(
+            bool_vec.iter().any(|b| *b)
+        )))
+    } else {
+        Err(RuntimeError{ })
+    }
 }
 
 fn unwrap_bool_token_vec(tokens: &Vec<Token>) -> Result<Vec<bool>, RuntimeError> {
@@ -41,4 +48,44 @@ fn it_should_reduce_trues_to_true() {
     let expected = Token::Operand(Type::Bool(true));
     let actual = reduce_and(&mut stack).expect("error on valid bool stack");
     assert!(expected == actual, "failed to reduce all trues to true with AND");
+}
+
+#[test]
+fn it_should_reduce_mixed_to_false() {
+    let mut stack = vec![
+        Token::Operand(Type::Bool(true)),
+        Token::Operand(Type::Bool(true)),
+        Token::Operand(Type::Bool(false)),
+        Token::Operand(Type::Bool(true)),
+    ];
+
+    let expected = Token::Operand(Type::Bool(false));
+    let actual = reduce_and(&mut stack).expect("error on valid bool stack");
+    assert!(expected == actual, "failed to reduce all false on mixed vec with AND");
+}
+
+#[test]
+fn it_should_reduce_mixed_to_true() {
+    let mut stack = vec![
+        Token::Operand(Type::Bool(true)),
+        Token::Operand(Type::Bool(false)),
+        Token::Operand(Type::Bool(false)),
+    ];
+
+    let expected = Token::Operand(Type::Bool(true));
+    let actual = reduce_or(&mut stack).expect("error on valid bool stack");
+    assert!(expected == actual, "failed to reduce mixed vec to true with OR");
+}
+
+#[test]
+fn it_should_reduce_falses_to_false() {
+    let mut stack = vec![
+        Token::Operand(Type::Bool(false)),
+        Token::Operand(Type::Bool(false)),
+        Token::Operand(Type::Bool(false)),
+    ];
+
+    let expected = Token::Operand(Type::Bool(false));
+    let actual = reduce_or(&mut stack).expect("error on valid bool stack");
+    assert!(expected == actual, "failed to reduce all false vec to false with OR");
 }
