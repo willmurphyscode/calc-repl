c4dc6ec8ca5c9c0971bf9cdda1558453a47726a9
Will Murphy <willmurphyscode@gmail.com>
2018-02-21 06:53:19 -0500

    Reverse stack in comparison reducers
    The argument to comparison reducers is built up by pushing onto a
    stack. Pushing is the easiest way to build up this vector, but we
    need to have the comparisons operate on the array in normal order,
    not in stack order. For now, we're just reversing in place.

--- src/interpreter/comparison_reducers.rs
+++ src/interpreter/comparison_reducers.rs
@@ -8,11 +8,10 @@ pub fn reduce_gt(stack: &mut Vec<Token>) -> Result<Token, RuntimeError> {
     if let Ok(integers) = integers_vec {
         if integers.len() >= 2 {
             let mut result = true;
-            integers.iter().fold(isize::MAX, |a, b| {
-                // intentionally reversed operator
-                // other stack operations will cause the operands to reach this 
-                // block in reverse order.
-                result = result && (a < *b);
+            // rev because what's coming in was built up by pushing a stack,
+            // but order matters for these comparators
+            integers.iter().rev().fold(isize::MAX, |a, b| {
+                result = result && (a > *b);
                 *b
             });
             return Ok(Token::Operand(Type::Bool(
@@ -28,11 +27,10 @@ pub fn reduce_lt(stack: &mut Vec<Token>) -> Result<Token, RuntimeError> {
     if let Ok(integers) = integers_vec {
         if integers.len() >= 2 {
             let mut result = true;
-            integers.iter().fold(isize::MIN, |a, b| {
-                // intentionally reversed operator
-                // other stack operations will cause the operands to reach this 
-                // block in reverse order.
-                result = result && (a > *b);
+            // rev because what's coming in was built up by pushing a stack,
+            // but order matters for these comparators
+            integers.iter().rev().fold(isize::MIN, |a, b| {
+                result = result && (a < *b);
                 *b
             });
             return Ok(Token::Operand(Type::Bool(
@@ -48,7 +46,7 @@ fn one_is_less_than_two() {
     let mut stack = vec![
         Token::Operand(Type::Integer(1)),
         Token::Operand(Type::Integer(2)),
-    ];
+    ].into_iter().rev().collect();
     let expected = Token::Operand(Type::Bool(true));
     let actual = reduce_lt(&mut stack).expect("failed to reduce stack for LT");
     assert!(expected == actual);
@@ -59,7 +57,7 @@ fn two_is_greater_than_one() {
     let mut stack = vec![
         Token::Operand(Type::Integer(2)),
         Token::Operand(Type::Integer(1)),
-    ];
+    ].into_iter().rev().collect();
     let expected = Token::Operand(Type::Bool(true));
     let actual = reduce_gt(&mut stack).expect("failed to reduce stack for LT");
     assert!(expected == actual);
